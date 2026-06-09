bc6491647a4ef6e092f07ef674a1ee19739077c6
PlacementPrep Developer <dev@placementprep.com>
2026-06-09 11:28:38 +0530

    Init: Add config.py with SQLAlchemy DB configurations

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-for-placement-prep-tracker-12345'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///placement_prep.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
