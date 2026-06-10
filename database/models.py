109f72bd83c4b772ff53d45a3bf65fa3d7d21c0a
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-11 00:35:33 +0530

    user details like streak_count, last activity date etc added

--- database/models.py
+++ database/models.py
@@ -3,3 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    streak_count = db.Column(db.Integer, default=0)
+    longest_streak = db.Column(db.Integer, default=0)
+    last_activity_date = db.Column(db.Date, nullable=True)
+    is_admin = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
