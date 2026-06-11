8442808f9373b670df7354b37d1c97290fcfbad7
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-11 15:44:55 +0530

    added

---
+++ auth/routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, request, redirect, url_for, flash, session, render_template
+from database.models import db, User
+from auth.forms import RegistrationForm, LoginForm, ProfileForm
+from dsa.services import log_activity
+from functools import wraps
+
