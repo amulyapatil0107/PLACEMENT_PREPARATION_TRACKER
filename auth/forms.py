257e8762dfc22c24dfa9def820a8fb1b08e35996
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-11 15:34:28 +0530

    username added to it

--- auth/forms.py
+++ auth/forms.py
@@ -1,2 +1,6 @@
 from wtforms import Form, StringField, PasswordField, EmailField, validators
 class RegistrationForm(Form):
+     username = StringField('Username', [
+        validators.Length(min=4, max=25),
+        validators.DataRequired()
+    ])
