1c23becf295a540a6c14c9f9bc8497f9fd20e801
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-12 22:48:37 +0530

    Feat: Add update_streak function helper skeleton in services.py

--- dsa/services.py
+++ dsa/services.py
@@ -1,2 +1,10 @@
 from datetime import datetime, date, timedelta
 from database.models import db, User, Activity
+
+def update_streak(user):
+    today = date.today()
+    if user.last_activity_date == today:
+        return
+    
+    if user.last_activity_date == today - timedelta(days=1):
+        user.streak_count += 1
