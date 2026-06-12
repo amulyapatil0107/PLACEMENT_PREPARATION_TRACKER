c9326db455201e3fa38f94730d105c319e133466
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-12 23:02:30 +0530

    Feat: Add imports and blueprint definition inside notes/routes.py

---
+++ notes/routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from database.models import db, Note
+from auth.routes import login_required
+from dsa.services import log_activity
