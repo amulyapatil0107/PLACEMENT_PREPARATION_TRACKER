e5b58f3a40b73b8c9badcf70b7eb7777672a8195
amulyapatil0107 <amulyapatil0107@gamil.com>
2026-06-12 22:48:37 +0530

    Feat: Complete streak calculation and update commit inside services.py

--- dsa/services.py
+++ dsa/services.py
@@ -8,3 +8,13 @@ def update_streak(user):
     
     if user.last_activity_date == today - timedelta(days=1):
         user.streak_count += 1
+        if user.streak_count > user.longest_streak:
+            user.longest_streak = user.streak_count
+    elif user.last_activity_date is None or user.last_activity_date < today - timedelta(days=1):
+        user.streak_count = 1
+        if user.longest_streak == 0:
+            user.longest_streak = 1
+            
+    user.last_activity_date = today
+    db.session.commit()
+
